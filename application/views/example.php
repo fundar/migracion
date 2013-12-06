5abf8b87742cd57f63077e01f2f4f763b71f5676
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-12-06 15:44:55 -0600

    title & keyword head

--- application/views/example.php
+++ application/views/example.php
@@ -2,13 +2,15 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-<?php 
-foreach($css_files as $file): ?>
-	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-<?php endforeach; ?>
-<?php foreach($js_files as $file): ?>
-	<script src="<?php echo $file; ?>"></script>
-<?php endforeach; ?>
+	<title>Migración y transparencia</title>
+	<meta name="keywords" content="Migración, transparencia" />
+    <meta name="description" content="Migración y transparencia" />
+	<?php foreach($css_files as $file): ?>
+		<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+	<?php endforeach; ?>
+	<?php foreach($js_files as $file): ?>
+		<script src="<?php echo $file; ?>"></script>
+	<?php endforeach; ?>
 <style type='text/css'>
 body
 {
