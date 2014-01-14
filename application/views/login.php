35304feab651e7eba91f8d5d814235fe391876d0
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-01-13 18:16:28 -0600

    login view fixed css & js

--- application/views/login.php
+++ application/views/login.php
@@ -5,12 +5,6 @@
 	<title>Migración y transparencia - Login</title>
 	<meta name="keywords" content="Migración, transparencia" />
     <meta name="description" content="Migración y transparencia" />
-	<?php foreach($css_files as $file): ?>
-		<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-	<?php endforeach; ?>
-	<?php foreach($js_files as $file): ?>
-		<script src="<?php echo $file; ?>"></script>
-	<?php endforeach; ?>
 <style type='text/css'>
 body
 {
