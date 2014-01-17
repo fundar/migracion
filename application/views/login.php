7b7c8e4ed8669644b357622e4785f8610d8c5d10
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-01-16 19:08:05 -0600

    login form type password

--- application/views/login.php
+++ application/views/login.php
@@ -26,7 +26,7 @@ a:hover
     <div>
 		<form action="" method="POST">
 			<p>email: <input type="text" name="email" id="email" /></p>
-			<p>password: <input type="text" name="pwd" id="pwd" /></p>
+			<p>password: <input type="password" name="pwd" id="pwd" /></p>
 			<p>
 				<input type="submit" name="submit" value="enviar" />
 			</p>
